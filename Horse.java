f4e83a16bfd1509ec5da28d7936cb223fc11ce97
aartikansal <60615341+aartikansal@users.noreply.github.com>
2020-10-21 19:33:04 -0400

    Add files via upload

---
+++ Horse.java
@@ -0,0 +1,14 @@
+
+/*
+Horse and Pegasus
+
+*/
+    public class Horse{
+    public static void main(String[] args) {
+    }
+    public class Horses {
+
+    }
+    public class Pegasus extends Horses {
+    }
+}
