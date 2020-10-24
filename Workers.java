716a72b60bece22399cefd388c243ed340727080
aartikansal <60615341+aartikansal@users.noreply.github.com>
2020-10-23 20:37:24 -0400

    Add files via upload

---
+++ Workers.java
@@ -0,0 +1,24 @@
+/*
+Hard workers
+*/
+
+public class Workers {
+    public static void main(String[] args) {
+    }
+
+    public class Manager extends Employee {
+
+    }
+
+    public class CEO extends Employee {
+
+    }
+
+    public class Employee {
+
+    }
+
+    public class Secretary extends Employee{
+
+    }
+}
