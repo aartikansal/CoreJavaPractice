028b5daeaa8dad965a3b4ee6589c118b73b3952c
aartikansal <60615341+aartikansal@users.noreply.github.com>
2020-10-23 20:39:18 -0400

    Add files via upload

---
+++ IT.java
@@ -0,0 +1,46 @@
+
+/*
+IT company
+
+*/
+
+public class IT {
+    public static void main(String[] args) {
+    }
+
+    public class Employee {
+
+    }
+
+    public class Clerk extends Employee {
+
+    }
+
+    public class ITSpecialist extends Employee {
+
+    }
+
+    public class Programmer extends ITSpecialist {
+
+    }
+
+    public class ProjectManager extends ITSpecialist {
+
+    }
+
+    public class CTO extends ITSpecialist {
+
+    }
+
+    public class OfficeManager extends Clerk {
+
+    }
+
+    public class Recruiter extends Clerk {
+
+    }
+
+    public class Custodian extends Clerk {
+
+    }
+}
