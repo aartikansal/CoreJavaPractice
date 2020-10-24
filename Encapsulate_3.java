ae673de3cd5988092d115f4e24fd38c4755aa336
aartikansal <60615341+aartikansal@users.noreply.github.com>
2020-10-24 16:27:37 -0400

    Add files via upload

---
+++ Encapsulate_3.java
@@ -0,0 +1,40 @@
+
+
+/*
+Don't forget to encapsulate
+*/
+
+public class Encapsulate_3 {
+    public static void main(String[] args) {
+    }
+
+    public class Cat {
+        private String name;
+        private int age;
+        private int weight;
+        private int speed;
+
+        public Cat(String name, int age, int weight, int speed) {
+            this.name = name;
+            this.age = age;
+            this.weight = weight;
+            this.speed = speed;
+        }
+
+        public String getName() {
+            return null;
+        }
+
+        public int getAge() {
+            return 0;
+        }
+
+        public void setWeight(int weight) {
+
+        }
+
+        public void setSpeed(int speed) {
+
+        }
+    }
+}
