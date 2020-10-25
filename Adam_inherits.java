5ee2ced11a2721339e00854a7d91793b3f81f62f
aartikansal <60615341+aartikansal@users.noreply.github.com>
2020-10-25 18:57:24 -0400

    Add files via upload

---
+++ Adam_inherits.java
@@ -0,0 +1,20 @@
+
+/*
+"He took one of his ribs"
+
+*/
+
+public class Adam_inherits {
+    public static void main(String[] args) {
+    }
+
+    // Adam
+    public class Adam {
+
+    }
+
+    // Eve
+    public class Eve extends Adam {
+
+    }
+}
