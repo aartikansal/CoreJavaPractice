df57f3a57416396565386aa1901535b694321b52
aartikansal <60615341+aartikansal@users.noreply.github.com>
2020-10-25 19:04:51 -0400

    Add files via upload

---
+++ Evolving_inheritance.java
@@ -0,0 +1,23 @@
+
+
+/*
+Evolving classes
+
+*/
+
+public class Evolving_inheritance {
+    public static void main(String[] args) {
+    }
+
+    public class Fish {
+
+    }
+
+    public class Lizard extends Fish {
+
+    }
+
+    public class Dinosaur extends Lizard {
+
+    }
+}
