865f7648fb36f0689396abea4889264bd8d4564d
aartikansal <60615341+aartikansal@users.noreply.github.com>
2020-10-25 19:07:12 -0400

    Add files via upload

---
+++ Slave_inherit.java
@@ -0,0 +1,27 @@
+
+/*
+From student to skilled slave
+
+*/
+
+public class Slave_inherit {
+    public static void main(String[] args) {
+    }
+
+    public class Schoolboy {
+
+    }
+
+    public class Student extends Schoolboy {
+
+    }
+
+    public class Employee extends Student {
+
+    }
+
+    public class Slave extends Employee {
+
+    }
+
+}
