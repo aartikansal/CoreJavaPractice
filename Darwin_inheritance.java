eb69c5927525ad5cf95d8ad6c31c8f1a86bf36b1
aartikansal <60615341+aartikansal@users.noreply.github.com>
2020-10-25 19:02:25 -0400

    Add files via upload

---
+++ Darwin_inheritance.java
@@ -0,0 +1,27 @@
+
+/*
+  Playing Darwin
+ */
+
+public class Darwin_inheritance {
+
+    public static void main(String[] args) {
+    }
+
+    public class Fish {
+
+    }
+
+    public class Animal extends Fish {
+
+    }
+
+    public class Ape extends Animal {
+
+    }
+
+    public class Human extends Ape {
+
+    }
+
+}
