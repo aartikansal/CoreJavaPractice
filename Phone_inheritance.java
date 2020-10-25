fea20402b9d7872ffa3f6ce978fe53707dcaff0c
aartikansal <60615341+aartikansal@users.noreply.github.com>
2020-10-25 18:59:30 -0400

    Add files via upload

---
+++ Phone_inheritance.java
@@ -0,0 +1,17 @@
+
+/*
+Frenemies
+*/
+
+public class Phone_inheritance {
+    public static void main(String[] args) {
+    }
+
+    public class AppleIPhone {
+
+    }
+
+    public class SamsungGalaxyS2 extends AppleIPhone {
+
+    }
+}
