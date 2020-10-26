918ee50b4a1910493b08b5a5976bedd4a23bf7ee
aartikansal <60615341+aartikansal@users.noreply.github.com>
2020-10-26 17:52:08 -0400

    Add files via upload

---
+++ Asset_inherits.java
@@ -0,0 +1,33 @@
+
+/*
+Correct inheritance chain: part 5
+*/
+
+public class Asset_inherits {
+    public static void main(String[] args) {
+    }
+
+    public class House extends Asset {
+
+    }
+
+    public class Cat extends Animal {
+
+    }
+
+    public class Car extends Asset {
+
+    }
+
+    public class Dog extends Animal {
+
+    }
+
+    public class Animal {
+
+    }
+
+    public class Asset {
+
+    }
+}
