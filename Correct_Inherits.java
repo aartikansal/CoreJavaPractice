5b8c3543312628c84bff01384d6c8354126c93c3
aartikansal <60615341+aartikansal@users.noreply.github.com>
2020-10-26 17:55:05 -0400

    Add files via upload

---
+++ Correct_Inherits.java
@@ -0,0 +1,31 @@
+
+
+/*
+Strange unfamiliar code
+
+*/
+
+public class Correct_Inherits {
+    public static void main(String[] args) {
+    }
+
+    public class Pet {
+
+    }
+
+    public class Cat extends Pet {
+
+    }
+
+    public class Dog extends Pet  {
+
+    }
+
+    public class House {
+
+    }
+
+    public class Airplane {
+
+    }
+}
