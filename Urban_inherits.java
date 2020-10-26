7cd7d05cb3045d885c6475d27f2835d4278816a4
aartikansal <60615341+aartikansal@users.noreply.github.com>
2020-10-26 17:44:45 -0400

    Add files via upload

---
+++ Urban_inherits.java
@@ -0,0 +1,26 @@
+
+
+/*
+Urban household
+*/
+
+public class Urban_inherits {
+    public static void main(String[] args) {
+    }
+
+    public class Pet {
+
+    }
+
+    public class Cat extends Pet {
+
+    }
+
+    public class Car {
+
+    }
+
+    public class Dog extends Pet {
+
+    }
+}
