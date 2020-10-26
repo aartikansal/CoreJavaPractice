b918d01d84f1b7eca63701d475a5007c30f14111
aartikansal <60615341+aartikansal@users.noreply.github.com>
2020-10-26 17:36:20 -0400

    Add files via upload

---
+++ Pet_inherits.java
@@ -0,0 +1,22 @@
+
+/*
+Inheritance chain
+
+*/
+
+public class Pet_inherits {
+    public static void main(String[] args) {
+    }
+
+    public class Pet {
+
+    }
+
+    public class Cat extends Pet {
+
+    }
+
+    public class Dog extends Pet {
+
+    }
+}
