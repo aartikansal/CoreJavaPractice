5b390212a7b26c22d1d885a442b72f3d7f01b126
aartikansal <60615341+aartikansal@users.noreply.github.com>
2020-10-26 17:58:36 -0400

    Add files via upload

---
+++ Chess_Pieces.java
@@ -0,0 +1,31 @@
+
+
+/*
+Save the chess club
+*/
+
+public class Chess_Pieces {
+    public static void main(String[] args) {
+    }
+    public class ChessPiece{
+
+    }
+
+    public class King extends ChessPiece {
+    }
+
+    public class Queen extends ChessPiece {
+    }
+
+    public class Rook extends ChessPiece {
+    }
+
+    public class Knight extends ChessPiece {
+    }
+
+    public class Bishop extends ChessPiece {
+    }
+
+    public class Pawn extends ChessPiece {
+    }
+}
