cf7c9b490fdaf7ea5f719e331818d729462fc749
aartikansal <60615341+aartikansal@users.noreply.github.com>
2020-10-26 20:47:39 -0400

    Add files via upload

---
+++ Whales_oride.java
@@ -0,0 +1,25 @@
+
+/*
+Whales and cows
+*/
+
+public class Whales_oride {
+    public static void main(String[] args) {
+        Cow cow = new Whale();
+
+        System.out.println(cow.getName());
+    }
+
+    public static class Cow {
+        public String getName() {
+            return "I'm a cow";
+        }
+    }
+
+    public static class Whale extends Cow {
+        public String getName(){
+            return "I'm not a cow. I'm a whale.";
+        }
+
+    }
+}
