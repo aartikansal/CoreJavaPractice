fa7423340ef0ddcfaeadc4a1f86dff0581fa4d48
aartikansal <60615341+aartikansal@users.noreply.github.com>
2020-10-27 21:14:09 -0400

    Add files via upload

---
+++ Overload_2.java
@@ -0,0 +1,20 @@
+
+
+/*
+int and Integer
+
+*/
+
+public class Overload_2 {
+    public static void main(String[] args) {
+        int a = 5;
+        print(a);
+        Integer b =7;
+        print(b);
+    }
+    public static void print(int x){
+
+    }
+    public static void print(Integer y){
+    }
+}
