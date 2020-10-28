0d39e3d5a3b6c525531cd6e735553685d4d07e76
aartikansal <60615341+aartikansal@users.noreply.github.com>
2020-10-27 21:18:06 -0400

    Add files via upload

---
+++ Overload_3.java
@@ -0,0 +1,25 @@
+
+/*
+Freedom of the press
+*/
+
+public class Overload_3 {
+    public static void main(String[] args) {
+
+    }
+    public static void print(int x){
+
+    }
+    public static void print(double y){
+
+    }
+    public static void print(Integer a){
+
+    }
+    public static void print(String a){
+
+    }
+    public static void print(float k){
+
+    }
+}
