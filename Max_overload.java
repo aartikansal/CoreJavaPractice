5bef8b5173b8f285c3dd945714969d44bf1868db
aartikansal <60615341+aartikansal@users.noreply.github.com>
2020-10-27 21:38:29 -0400

    Add files via upload

---
+++ Max_overload.java
@@ -0,0 +1,36 @@
+
+/*
+Three methods and a maximum
+
+*/
+
+public class Max_overload {
+    public static void main(String[] args) {
+
+    }
+    public static  int max(int a,int b){
+        if(a>b){
+            return a;
+        }
+        else {
+            return b;
+        }
+    }
+    public static long max(long z, long u){
+        if(z>u){
+            return z;
+        }
+        else {
+            return u;
+        }
+    }
+    public static double max(double x, double y){
+        if(x>y)
+        {
+            return x;
+        }
+        else {
+            return y;
+        }
+    }
+}
