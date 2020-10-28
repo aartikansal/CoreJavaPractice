1891b0c08232fb821d0a76509be5b637092d6a3c
aartikansal <60615341+aartikansal@users.noreply.github.com>
2020-10-27 21:00:13 -0400

    Add files via upload

---
+++ Overload_1.java
@@ -0,0 +1,15 @@
+
+
+public class Overload_1 {
+    public static void main(String[] args) {
+    }
+
+    public static int print(int b) {
+        return b;
+
+    }
+
+    public static String print(String x) {
+        return x;
+    }
+}
