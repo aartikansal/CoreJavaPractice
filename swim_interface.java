665813cb7539498a8ca9e9ad8f2ef87a993302e7
aartikansal <60615341+aartikansal@users.noreply.github.com>
2020-10-29 19:38:30 -0400

    Add files via upload

---
+++ swim_interface.java
@@ -0,0 +1,21 @@
+
+/*
+Fly, run and swim
+*/
+
+public class swim_interface {
+    public static void main(String[] args) {
+
+    }
+
+    public interface CanFly{
+        void fly();
+    }
+    public interface CanRun{
+        void run();
+    }
+    public interface CanSwim{
+        void swim();
+    }
+
+}
