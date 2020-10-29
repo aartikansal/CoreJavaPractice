625d541b6a1f17f1c8198839c9949f76296eed6f
aartikansal <60615341+aartikansal@users.noreply.github.com>
2020-10-28 20:30:21 -0400

    Add files via upload

---
+++ Pet_abstract.java
@@ -0,0 +1,16 @@
+
+
+//Abstract class//
+public class Pet_abstract {
+    public static void main(String[] args) {
+
+    }
+
+    public static abstract class Pet {
+        public String getName() {
+            return "I'm a kitten";
+        }
+    }
+
+}
+
