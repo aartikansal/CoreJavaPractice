46dbf5773cb67f88d5b80b6e4961e5101f7ff573
aartikansal <60615341+aartikansal@users.noreply.github.com>
2020-10-29 22:42:25 -0400

    Add files via upload

---
+++ Fluffy_override.java
@@ -0,0 +1,38 @@
+
+
+/*
+Fluffy no more
+
+*/
+
+public class Fluffy_override {
+    public static void main(String[] args) {
+        Pet pet = new Cat();
+        pet.setName("I'm Fluffy");
+
+        System.out.println(pet.getName());
+    }
+
+    public static class Pet {
+        protected String name;
+
+        public Pet() {
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+    }
+
+    public static class Cat extends Pet {
+        public void setName(String name){
+            super.setName("I'm a cat");
+        }
+
+    }
+}
