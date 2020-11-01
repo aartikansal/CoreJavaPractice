f0ce0f021d19d026b4c9c3e54cd3e9e138a6eeef
aartikansal <60615341+aartikansal@users.noreply.github.com>
2020-10-31 20:54:43 -0400

    Add files via upload

---
+++ Pegasus_more.java
@@ -0,0 +1,33 @@
+
+
+/*
+Adding new functionality
+
+*/
+
+public class Pegasus_more {
+    public static void main(String[] args) {
+        Pegasus horse = new Pegasus();
+    }
+
+    public static interface CanFly {
+        public void fly();
+    }
+
+    public static class Horse {
+        public void run() {
+
+        }
+    }
+
+    public static class Pegasus extends Horse implements CanFly {
+        @Override
+        public void run(){
+
+        }
+        @Override
+        public void fly(){
+
+        }
+    }
+}
