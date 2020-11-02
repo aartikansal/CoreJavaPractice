f0e9a85c0c3b5f9fbeaf8165a4890e46bb13b050
aartikansal <60615341+aartikansal@users.noreply.github.com>
2020-11-02 12:19:50 -0500

    Add files via upload

---
+++ Beer_interface.java
@@ -0,0 +1,40 @@
+
+/*
+Beer
+*/
+
+public class Beer_interface {
+    public static void main(String[] args) throws Exception {
+    }
+
+    public interface Drinker {
+        void askForMore(String message);
+
+        void sayThankYou();
+
+        boolean isReadyToGoHome();
+    }
+
+    public interface Alcoholic extends Drinker {
+        boolean READY_TO_GO_HOME = false;
+
+        void sleepOnTheFloor();
+    }
+
+    public static class BeerLover implements Alcoholic {
+
+        public boolean isReadyToGoHome(){
+           return  READY_TO_GO_HOME ;
+        }
+
+        public void sleepOnTheFloor(){
+
+        }
+        public void askForMore(String message){
+
+        }
+        public void sayThankYou(){
+
+        }
+    }
+}
