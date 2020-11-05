e965bdfbd888049b88ada1e0d4dd7abeb24878cf
aartikansal <60615341+aartikansal@users.noreply.github.com>
2020-11-04 23:55:03 -0500

    Add files via upload

--- Tom_interface.java
+++ Tom_interface.java
@@ -1,27 +1,22 @@
 
 
-
 /*
 Tom, Jerry and Spike
-
 */
 
 public class Tom_interface {
     public static void main(String[] args) {
 
     }
-
-    // Can move
     public interface CanMove {
         void move();
     }
 
-    // Can be eaten
     public interface Edible {
         void beEaten();
     }
 
-    // Can eat
+
     public interface CanEat {
         void eat();
     }
