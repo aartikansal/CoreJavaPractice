9c9cf475775f1200cd01986a2bddcc478265a8f6
aartikansal <60615341+aartikansal@users.noreply.github.com>
2020-12-03 18:46:00 -0500

    Add files via upload

---
+++ stringbuilder_streams.java
@@ -0,0 +1,30 @@
+
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.io.FileInputStream;
+
+/*
+Reading a file
+
+*/
+public class stringbuilder_streams {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String sourceFileName = reader.readLine();
+        FileInputStream fileInputStream = new FileInputStream(sourceFileName);
+
+        Scanner scanner = new Scanner(fileInputStream);
+        StringBuilder builder = new StringBuilder();
+
+        while (scanner.hasNextLine()) {
+            builder.append(scanner.nextLine()).append("\n");
+        }
+
+        System.out.print(builder.toString());
+
+        scanner.close();
+        reader.close();
+    }
+}
