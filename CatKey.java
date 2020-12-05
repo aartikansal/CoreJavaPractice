dfdf9bc76f67378a975c91556b10e3439b869754
aartikansal <60615341+aartikansal@users.noreply.github.com>
2020-12-05 17:22:34 -0500

    Add files via upload

---
+++ CatKey.java
@@ -0,0 +1,61 @@
+
+/*
+Cats
+*/
+
+public class CatKey {
+    public static void main(String[] args) throws Exception {
+    }
+
+    static class CatFactory {
+        static Cat getCatByKey(String key) {
+            Cat cat = null;
+            if ("feral".equals(key)) {
+                cat = new MeanCat("Claws");
+            } else if ("miss".equals(key)) {
+                cat = new NiceCat("Missy");
+            } else if ("smudge".equals(key)) {
+                cat = new NiceCat("Smudgey");
+            } else {
+                cat = new Cat(key);
+            }
+            return cat;
+        }
+    }
+
+    static class Cat {
+        private String name;
+
+        protected Cat(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return this.name;
+        }
+
+        public String toString() {
+            return "I'm " + getName() + ", an alley cat";
+        }
+    }
+
+    static class MeanCat extends Cat {
+        MeanCat(String name) {
+            super(name);
+        }
+
+        public String toString() {
+            return "I'm " + getName() + ", and I'm going to claw your eyes out";
+        }
+    }
+
+    static class NiceCat extends Cat {
+        NiceCat(String name) {
+            super(name);
+        }
+
+        public String toString() {
+            return "I'm a nice kitten named " + getName();
+        }
+    }
+}
