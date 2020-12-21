114344ccbfd68ab3ef902d77a60b7356f84c6230
aartikansal <60615341+aartikansal@users.noreply.github.com>
2020-12-20 21:26:24 -0500

    Add files via upload

---
+++ Inheritance_constructor.java
@@ -0,0 +1,17 @@
+public class Inheritance_constructor {
+    public static void main(String[] args) {
+    }
+
+    public interface Runnable {
+
+    }
+
+    public class Machine implements Runnable {
+
+    }
+
+    public class Car extends Machine {
+
+    }
+}
+
