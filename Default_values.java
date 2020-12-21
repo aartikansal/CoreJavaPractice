0ec82b362386d550280e1be06226122c183bf766
aartikansal <60615341+aartikansal@users.noreply.github.com>
2020-12-21 18:38:49 -0500

    Add files via upload

---
+++ Default_values.java
@@ -0,0 +1,21 @@
+public class Default_values {
+    int intVar;
+    double doubleVar;
+    Double DoubleVar;
+    boolean booleanVar;
+    Object ObjectVar;
+    Exception ExceptionVar;
+    String StringVar;
+
+    public static void main(String[] args) {
+        Default_values default_values = new Default_values();
+        System.out.println(default_values.intVar);
+        System.out.println(default_values.doubleVar);
+        System.out.println(default_values.DoubleVar);
+        System.out.println(default_values.booleanVar);
+        System.out.println(default_values.ObjectVar);
+        System.out.println(default_values.ExceptionVar);
+        System.out.println(default_values.StringVar);
+
+    }
+}
