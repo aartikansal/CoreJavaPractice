0eaaaec30f77374ad7d809a1bdb9f11b3d1465fd
aartikansal <60615341+aartikansal@users.noreply.github.com>
2020-12-21 18:49:58 -0500

    Add files via upload

---
+++ Exception_static.java
@@ -0,0 +1,19 @@
+
+
+/*
+Static modifiers and exceptions
+*/
+
+public class Exception_static {
+    public static int A = 0;
+
+    static {
+        if (true) throw new RuntimeException("");
+    }
+
+    public static int B = 5;
+
+    public static void main(String[] args) {
+        System.out.println(B);
+    }
+}
