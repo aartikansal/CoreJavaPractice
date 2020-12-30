94c8d3252f2ac28058ccbad2163cd75141427331
aartikansal <60615341+aartikansal@users.noreply.github.com>
2020-12-29 21:13:32 -0500

    Add files via upload

---
+++ Musical_thread.java
@@ -0,0 +1,55 @@
+
+import java.util.Date;
+
+/*
+Let's talk music
+*/
+
+public class Musical_thread {
+    public static int delay = 1000;
+
+    public static void main(String[] args) {
+        Thread violin = new Thread(new Violin("Player"));
+        violin.start();
+    }
+
+    public static void sleepNSeconds(int n) {
+        try {
+            Thread.sleep(n * delay);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public interface MusicalInstrument extends Runnable {
+        Date startPlaying();
+
+        Date stopPlaying();
+    }
+
+    public static class Violin implements MusicalInstrument {
+        private String owner;
+
+        public Violin(String owner) {
+            this.owner = owner;
+        }
+
+        public Date startPlaying() {
+            System.out.println(this.owner + " is starting to play");
+            return new Date();
+        }
+
+        public Date stopPlaying() {
+            System.out.println(this.owner + " is stopping playing");
+            return new Date();
+        }
+
+        @Override
+        public void run() {
+            Date date1 = startPlaying();
+            sleepNSeconds(1);
+            Date date2 = stopPlaying();
+            System.out.println("Played for " + (date2.getTime() - date1.getTime()) + " ms");
+        }
+    }
+}
