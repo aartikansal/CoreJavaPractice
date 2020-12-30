8c5c441804bfb20235ca2fd75105033a5353a0b2
aartikansal <60615341+aartikansal@users.noreply.github.com>
2020-12-29 21:03:15 -0500

    Add files via upload

---
+++ Threads_lists.java
@@ -0,0 +1,26 @@
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+A list and some threads
+*/
+
+public class Threads_lists {
+    public static volatile List<Thread> list = new ArrayList<>(5);
+
+    public static void main(String[] args) {
+        list.add(new Thread(new SpecialThread()));
+        list.add(new Thread(new SpecialThread()));
+        list.add(new Thread(new SpecialThread()));
+        list.add(new Thread(new SpecialThread()));
+        list.add(new Thread(new SpecialThread()));
+    }
+
+    public static class SpecialThread implements Runnable {
+        public void run() {
+            System.out.println("This is the run method inside SpecialThread");
+        }
+    }
+}
