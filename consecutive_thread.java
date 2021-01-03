7ea68beb073f5dd3d27f285ac659072f06436e84
aartikansal <60615341+aartikansal@users.noreply.github.com>
2021-01-03 17:56:11 -0500

    Add files via upload

---
+++ consecutive_thread.java
@@ -0,0 +1,39 @@
+
+
+
+/*
+Consecutive threads
+
+*/
+
+public class consecutive_thread {
+        public static MyThread t = new MyThread();
+        static String message = "inside main ";
+
+        public static void main(String a[]) throws Exception {
+            t.start();
+            t.join();
+            for (int i = 0; i < 10; i++) {
+                System.out.println(message + i);
+                sleep();
+            }
+        }
+
+        public static void sleep() {
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+            }
+        }
+
+        static class MyThread extends Thread {
+            String message = "inside MyThread ";
+
+            public void run() {
+                for (int i = 0; i < 10; i++) {
+                    System.out.println(message + i);
+                    consecutive_thread.sleep();
+                }
+            }
+        }
+    }
