c1a2879e09e028a4b47dbb6f9ba901fa6acf3f5c
aartikansal <60615341+aartikansal@users.noreply.github.com>
2021-01-03 17:52:27 -0500

    Add files via upload

---
+++ Recursive_Thread.java
@@ -0,0 +1,34 @@
+
+
+
+/*
+Creating threads recursively
+
+*/
+
+public class Recursive_Thread {
+    static int count = 15;
+    static volatile int createdThreadCount;
+
+    public static void main(String[] args) {
+        System.out.println(new GenerateThread());
+    }
+
+    public static class GenerateThread extends Thread {
+        public GenerateThread() {
+            super(String.valueOf(++createdThreadCount));
+            start();
+        }
+
+        public void run() {
+            if (createdThreadCount < count) {
+                System.out.println(new GenerateThread());
+            }
+        }
+
+        @Override
+        public String toString() {
+            return getName() + " created";
+        }
+    }
+}
